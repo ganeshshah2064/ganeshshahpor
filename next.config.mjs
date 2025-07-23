b8459fb4f95ae8f9e3a1c6ce2848cfa066b7a46b
Ganesh Shah <ganesh.ffx@gmail.com>
2025-07-23 18:51:57 +0545

    Add files via upload

---
+++ next.config.mjs
@@ -0,0 +1,19 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**',
+      },
+    ],
+    dangerouslyAllowSVG: true,
+    contentDispositionType: 'attachment',
+    contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
+  },
+  experimental: {
+    optimizePackageImports: ['lucide-react'],
+  },
+};
+
+export default nextConfig;
